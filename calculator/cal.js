a3c8f160b3f4f77a5e63094e77acde420f8941c1
shiv103 <143366926+shiv103@users.noreply.github.com>
2023-09-24 16:11:10 +0530

    Add files via upload

---
+++ calculator/cal.js
@@ -0,0 +1,26 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function displays the values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns the result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+function display(value) {
+    document.getElementById("result").value += value;
+}
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
